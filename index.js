c327bfa46afbd05cd636b2a01f6cd1de0d51ccc0
OfficialShehzad <shehzadkhanofficial01@gmail.com>
2022-11-02 00:40:28 +0530

    added all the sections (responsiveness pending)

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // document.documentElement.style.scrollBehavior = "smooth";
+
+let copyButton = document.querySelector('.copy-number')
+let copyConfirmationBox = document.querySelector('.copy-confirmation')
+
+copyButton.addEventListener("click", (e) => {
+    console.log(e.target)
+
+    let copyText = document.getElementById('ph-number')
+    console.log(copyText.innerText)
+
+    navigator.clipboard.writeText(copyText.innerHTML);
+    copyConfirmationBox.innerText = "Copied!"
+
+    setTimeout(() => {
+        copyConfirmationBox.innerHTML = ""
+    }, 5000)
+})
